1c3e16cba036c622f92180ce00673123874c1e7b
Sharipov <pavel.sharipov@hotmail.com>
2016-11-07 10:06:30 +0200

    added tests to TObjectServiceTest

--- src/test/java/com/sharipov/controller/TObjectServiceTest.java
+++ src/test/java/com/sharipov/controller/TObjectServiceTest.java
@@ -31,22 +31,22 @@ public class TObjectServiceTest {
 
 	@Test
 	public void contains_NonCorrectIdNumber_Success() {
-		testEntityManager.persist(new TObject("test title 1", 1L));
-		testEntityManager.persist(new TObject("test title 2", 2L));
+		testEntityManager.persist(new TObject("test title 1", 1));
+		testEntityManager.persist(new TObject("test title 2", 2));
 		boolean isContains = tObjectService.contains(3);
 		assertEquals(false, isContains);
 	}
 	@Test
 	public void contains_CorrectIdNumber_Success() {
-		TObject object = new TObject("test title 1", 1L);
+		TObject object = new TObject("test title 1", 1);
 		testEntityManager.persist(object);
 		boolean isContains = tObjectService.contains((int)testEntityManager.getId(object));
 		assertEquals(true, isContains);
 	}
 	@Test
 	public void finadAll_Success() {
-		testEntityManager.persist(new TObject("test title 1", 1L));
-		testEntityManager.persist(new TObject("test title 2", 2L));
+		testEntityManager.persist(new TObject("test title 1", 1));
+		testEntityManager.persist(new TObject("test title 2", 2));
 		List<TObject> list = tObjectService.findAll();
 		List<TObject> repositoryList = tObjectRepository.findAll();
 		assertArrayEquals(Arrays.array(repositoryList),Arrays.array(list));
@@ -59,7 +59,7 @@ public class TObjectServiceTest {
 	
 	@Test
 	public void save_IdIsEqualsZero_Success() {
-		TObject object = new TObject(0, "test title save", 1L);
+		TObject object = new TObject(0, "test title save", 1);
 		tObjectService.save(object, 0);
 		int lastIndex = (int) testEntityManager.getId(object);
 		TObject repositoryObject = testEntityManager.find(TObject.class, lastIndex);
@@ -68,17 +68,48 @@ public class TObjectServiceTest {
 	}
 	@Test
 	public void save_IdIsNotEqualsZero_Success() {
-		testEntityManager.persist(new TObject("test title 1", 1L));
-		testEntityManager.persist(new TObject("test title 2", 2L));
-		TObject object = new TObject(2, "test title update", 1L);
-		tObjectService.save(object, 2);
+		TObject object = new TObject("test title update", 1);
+		testEntityManager.persist(object);
+		int objectId = object.getId();
+		tObjectService.save(object, objectId);
 		int lastIndex = (int) testEntityManager.getId(object);
 		TObject repositoryObject = testEntityManager.find(TObject.class, lastIndex);
 		assertEquals("test title update", repositoryObject.getTitle());
 		assertNotEquals(0, repositoryObject.getId());
 	}
 	
+	@Test
+	public void findOne_CorrectId_Success() {
+		TObject object = new TObject("test title findOne", 5);
+		testEntityManager.persist(object);
+		int objectId = object.getId();
+		TObject repositoryObject = tObjectService.findOne(objectId);
+		assertEquals(object, repositoryObject);
+	}
+	@Test
+	public void findOne_NonCorrectId_Success() {
+		TObject object = new TObject("test title findOne", 5);
+		testEntityManager.persist(object);
+		int objectId = 100;
+		TObject repositoryObject = tObjectService.findOne(objectId);
+		assertNull(repositoryObject);
+		assertNotEquals(object, repositoryObject);
+	}
 	
+	@Test
+	public void delete_CorrectId_Success() {
+		TObject object1 = new TObject("test title delete1", 1);
+		TObject object2 = new TObject("test title delete2", 2);
+		testEntityManager.persist(object1);
+		testEntityManager.persist(object2);
+		int object1Id = object1.getId();
+		int object2Id = object2.getId();
+		TObject repositoryObject = tObjectService.findOne(object1Id);
+		tObjectService.delete(object2Id);
+		assertNull(tObjectService.findOne(object2Id));
+		assertNotNull(repositoryObject);
+		
+	}
 	
 	
 
